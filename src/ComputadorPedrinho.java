aa703c9d875872964ea8e383db74d94a8e649dbe
Maurício <mauro.cavalcante18@gmail.com>
2024-06-19 19:41:33 -0300

    exemplo Herança

--- src/ComputadorPedrinho.java
+++ src/ComputadorPedrinho.java
@@ -1,11 +1,29 @@
 public class ComputadorPedrinho {
 	public static void main(String[] args) {
-		//abrindo MSN Messenger
+		
+
+		System.out.println("MSN");
 		MSNMessenger msn = new MSNMessenger();
-		
-		
 		msn.enviarMensagem();
-		
 		msn.receberMensagem();
+
+		
+		
+
+		System.out.println("Facebook");
+		FacebookMessenger fbm = new FacebookMessenger();
+		fbm.enviarMensagem();
+		fbm.receberMensagem();
+
+		
+		
+
+		System.out.println("Telegram");
+
+		Telegram tlg = new Telegram();
+		tlg.enviarMensagem();
+		tlg.receberMensagem();		
+
+		
 	}
 }
